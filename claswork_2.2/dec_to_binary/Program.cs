6cf4a6b47e4bae0c2bbb170eb40365834b712c06
Danail Dimitrov <danaildimitrov321@gmail.com>
2019-01-20 12:22:02 +0200

    adding the materials

---
+++ claswork_2.2/dec_to_binary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dec_to_binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dec = int.Parse(Console.ReadLine());
+            //binary
+            string result = "";
+            while (dec > 0)
+            {
+                result += dec % 2;
+                dec /= 2;
+            }
+            Console.WriteLine("binary "+new string(result.Reverse().ToArray()));
+        }
+    }
+}
