1ce00c7ca22522fb62062b65ca5a0e6aff38f8a5
Danail Dimitrov <danaildimitrov321@gmail.com>
2019-02-10 12:08:24 +0200

    more tasks

---
+++ Lists and arrays/exercise/Lists/RemoveNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int lastNumber = nums.Last();
+
+            while (nums.Contains(lastNumber))
+            {
+                nums.Remove(lastNumber);
+            }
+            Console.Write(string.Join(", ", nums));
+            Console.WriteLine(" ");
+        } 
+    }
+}
