5441d0f0ac16e08c87e55f0bd7b871a972d2abf8
Danail Dimitrov <danaildimitrov321@gmail.com>
2019-03-09 10:06:13 +0200

    idk

---
+++ Methods/exercises/drawingSquare/drawingSquare/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drawingSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+            FirstAndLast(n);
+        }
+
+        static void FirstAndLast(byte n)
+        {
+            
+        }
+    }
+}
