3cc37961a06cefda1bd3d7a98ec52e702a2c0af6
Danail Dimitrov <danaildimitrov321@gmail.com>
2019-03-10 12:24:58 +0200

    Dictionaryes

---
+++ Dictionaryes/tasks/dict/phonebook/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+
+                string[] Args = command.Split(' ').ToArray();
+                if (Args[0] == "A")
+                {
+
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
