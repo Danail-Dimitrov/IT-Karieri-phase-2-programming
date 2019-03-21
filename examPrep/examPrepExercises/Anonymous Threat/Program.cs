f960b89717de8c58246d0273484ab47d3d82f128
Danail Dimitrov <danaildimitrov321@gmail.com>
2019-03-21 23:12:31 +0200

    .

---
+++ examPrep/examPrepExercises/Anonymous Threat/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_Threat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split(' ').ToList();
+            string[] comandArgs= Console.ReadLine().Split(' ').ToArray();
+
+            while (true)
+            {      
+                if (comandArgs[0] == "merge")
+                {
+                    input = Merge(input, comandArgs);
+                }
+                else if (comandArgs[0] == "divide")
+                {
+                    input = Divide(input, comandArgs);
+                }
+                else
+                {
+                    break;
+                }
+                comandArgs = Console.ReadLine().Split(' ').ToArray();
+            }
+            Console.WriteLine(string.Join(" ",input));
+        }
+
+        private static List<string> Divide(List<string> input, string[] comandArgs)
+        {
+            int index = int.Parse(comandArgs[1]);
+            int partitions = int.Parse(comandArgs[2]);
+            int charInObj,charInObjMax,charInObjMin;
+            string[] toInput = new string[partitions];
+            string workWith = input[index];
+            int a = 0;
+            int z = 0;
+
+            if (workWith.Length % partitions == 0)
+            {
+               charInObj = (int)(workWith.Length / partitions);               
+                for (int i = 0; i < partitions; i++)
+                {
+                    string b = string.Empty;                    
+                    for (int j = 0; j < charInObj; j++) //a b cd efgh ijkl
+                    {
+                        b += workWith[ j + z];                       
+                    }
+                    z+=b.Length;
+                    toInput[a] = b;
+                    a++;
+                }
+            }
+            else
+            {
+                double d = Math.Floor((double)(workWith.Length / partitions));
+                charInObjMin = (int)(d);
+
+                int m = charInObjMin * (partitions - 1);
+                charInObjMax = workWith.Length - m;
+
+                for (int i = 0; i < partitions-1; i++)
+                {
+                    string c = string.Empty;
+                    for (int j = 0; j < charInObjMin; j++) //a b cd efgh ijkl
+                    {
+                        c += workWith[j +z];
+                    }
+                    z += c.Length;
+                    toInput[a] = c;
+                    a++;
+                }
+                string f = string.Empty;
+                for (int j = 0; j < charInObjMax; j++) 
+                {                  
+                    f += workWith[workWith.Length-charInObjMax+ j];
+                }
+                toInput[a] = f;
+
+            }
+            input.RemoveAt(index);
+            for (int i = 0; i < partitions; i++)
+            {
+                input.Insert(index + i, toInput[i]);
+            }
+
+            return input;
+        }
+
+        private static List<string> Merge (List<string> input, string[] comandArgs)
+        {
+            int startIndex = int.Parse(comandArgs[1]);
+            int endIndex = int.Parse(comandArgs[2]);
+            string toInput = string.Empty;
+
+            if (startIndex<0)
+            {
+                startIndex = 0;
+            }
+            else if (startIndex > input.Count-1)
+            {
+                startIndex = input.Count - 1;
+            }
+
+            if (endIndex < 0)
+            {
+                endIndex = 0;
+            }
+            else if (endIndex > input.Count - 1)
+            {
+               endIndex = input.Count - 1;
+            }
+
+            int[] toRemove = new int[endIndex-startIndex+1];//a b cd efgh ijkl
+            int j = 0;
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                toInput += input[i];
+                toRemove[j] = i;
+                j++;
+            }
+
+            int a = toRemove.Length;
+
+            for (int i = 0; i < a-1; i++)
+            {
+                input.RemoveAt(startIndex);
+            }
+
+            input[startIndex] = toInput;
+
+            return input;
+        }
+    }
+}
---
+++ examPrep/examPrepExercises/Phoenix Grid/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace Phoenix_Grid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string[] input = a.Split('.').ToArray();
+            while (input[0] != "ReadMe")
+            {
+                bool isPalindrome = IsPalindrome(a);
+                if ( isPalindrome && !a.Contains(" ") && !a.Contains("_"))
+                {
+                    Console.WriteLine("YES");
+                }
+                else if (input[0] == " " && input.Length % 2 == 0)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+                a = Console.ReadLine();
+                input = a.Split('.').ToArray();
+            }
+        }
+
+        private static bool IsPalindrome(string a)
+        {
+            string reversed = Reverse(a);
+            if (a == reversed)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static string Reverse(string a)
+        {
+            char[] charArray = a.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
+
